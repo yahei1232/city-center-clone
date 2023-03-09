e604f2a39e0f9a38a4dc417d0445b13d865e1f5d
yahei1232 <yaheiyahei693@gmail.com>
2023-03-09 15:24:41 +0300

    create user file add add editUser function to user file in backend

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -15,3 +15,48 @@ export const getUsers = (req, res) => {
         }
     });
 };
+
+export const editUser = (req, res) => {
+    const token = req.cookies.accessToken;
+
+    if (!token) {
+        return res.status(401).json("Not logged in!");
+    }
+
+    jwt.verify(token, "secretkey", async (err, userInfo) => {
+        if (err) {
+            return res.status(500).json(err);
+        }
+
+        const { username, gmail, addrees, city, photo, phone } = req.body;
+
+        if (req.body.password) {
+            const salt = await bcrypt.genSalt(10);
+            req.body.password = await bcrypt.hash(req.body.password, salt)
+        }
+
+        const q = `
+        UPDATE user 
+        SET 
+            username = '${username}',
+            gmail = '${gmail}',
+            addrees = '${addrees}',
+            ${req.body.password ? `password = '${req.body.password}',` : ''}
+            addrees = '${addrees}',
+            city = '${city}',
+            photo = '${photo}',
+            phone = '${phone}'
+        WHERE 
+            id = ${userInfo.id};
+        `;
+
+        db.query(q, (err, data) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json(err);
+            }
+
+            return res.json("User has been updated.");
+        });
+    });
+};
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getUsers } from "../controllers/user.js";
+import { getUsers, editUser } from "../controllers/user.js";
 
 const router = express.Router();
 
 router.get("/", getUsers);
+router.put("/", editUser);
 
 export default router;
