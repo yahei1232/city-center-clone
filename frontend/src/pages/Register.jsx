743144458544090772dfd81148e7b83b8db7e7cc
yahei1232 <yaheiyahei693@gmail.com>
2023-03-11 13:46:52 +0300

    create reguster by axios in frontend

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,28 +1,63 @@
-import React from "react";
-import { Link } from 'react-router-dom'
+import React from 'react'
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useState } from 'react';
 
 function Register() {
+
+    const [inputs, setInputs] = useState({
+        username: "",
+        gmail: "",
+        password: "",
+        addrees: "",
+        city: "",
+        phone: "",
+    });
+
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        console.log(e.target.name)
+        setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.post("http://localhost:8800/api/auth/register", inputs, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            navigate("/login");
+        } catch (err) {
+            console.log("err");
+            console.log(err);
+        }
+    };
+
+
     return (
-        <div className="login">
+        <div className='login'>
             <h1>Account Reguster</h1>
-            <form className="auth row">
+            <form className="auth">
+
                 <div className="welcome">
                     <h2>Welcome To City Center</h2>
-                    <p>If you have an account click here.</p>
-                    <Link className='link' to='/login'>LOGIN</Link>
+                    <p>If you have an account click here</p><Link className='link' to="/login">LOGIN</Link>
                 </div>
-                <div className="inputs column">
-                    <input type="username" id="input-field" placeholder="username" />
-                    <input type="email" id="input-field" placeholder="Email" />
-                    <input type="password" id="input-field" placeholder="Password" />
-                    <input type="Adrees" id="input-field" placeholder="Adrees" />
-                    <input type="Telephone" id="input-field" placeholder="Telephone" />
-                    <input type="City" id="input-field" placeholder="City" />
-                    <input className="submit" type="submit" />
+                <div className="inputs">
+                    <input onChange={handleChange} type="username" name="username" id="input-field" placeholder='username' />
+                    <input onChange={handleChange} type="gmail" name="gmail" id="input-field" placeholder='gmail' />
+                    <input onChange={handleChange} type="password" name="password" id="input-field" placeholder='Password' />
+                    <input onChange={handleChange} type="text" name="addrees" id="input-field" placeholder='addrees' />
+                    <input onChange={handleChange} type="text" name="city" id="input-field" placeholder='city' />
+                    <input onChange={handleChange} type="number" name="phone" id="input-field" placeholder='phone' />
+                    <input onClick={handleSubmit} className='submit' type="submit" />
                 </div>
             </form>
         </div>
-    );
+    )
 }
 
-export default Register;
+export default Register
